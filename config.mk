f98be167faca9ee1f0ecf9aad61678367fea1623
spkal01 <kalligeross@gmail.com>
2021-04-20 21:23:00 +0530

    OPApps: initial import of OnePlus Apps
    Co-authored-by: MrSluffy <droopsdroops27@gmail.com>

---
+++ config.mk
@@ -0,0 +1,18 @@
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
+PRODUCT_COPY_FILES += \
+    vendor/opstuff/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-launcher.xml \
+    vendor/opstuff/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml
+
+PRODUCT_PACKAGES += \
+    OnePlusRecentsProvider \
+    OPLauncher 
+    
+ifneq ($(WITH_MINIMAL_OPAPPS), true)
+PRODUCT_PACKAGES += \
+    OPGallery \
+    OPIconpackOxygen \
+    OPIconpackRound \
+    OPWidget \
+    OPWeather
+endif
